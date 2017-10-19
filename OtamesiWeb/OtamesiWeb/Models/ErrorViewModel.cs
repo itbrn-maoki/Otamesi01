304eb0194cc077ede26f78a3ec8eb8758a775366
maoki <maoki@it-brn.com>
2017-10-19 14:35:53 +0900

    登録

---
+++ OtamesiWeb/OtamesiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OtamesiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
